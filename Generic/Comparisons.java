a13c9e369e75915550f16ea48452658c0b1f83af
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-03 04:49:47 +0000

    Create Generic/Comparisons.java

---
+++ Generic/Comparisons.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+    String one = "hello world";
+    String two = "hello world";
+
+    if (one.equals(two)) {
+      System.out.println("They are the same");
+    }
+
+    Object o = one;
+    Object t = two;
+
+    if (o.equals(t)) {
+      System.out.println("They are the same");
+    }
+
+    /*
+    Monkey m = new Monkey();
+    Monkey n = new Monkey();
+    
+    if (m.equals(n)) {
+      System.out.println("They are the same");
+    }
+    */
+  }
+}
