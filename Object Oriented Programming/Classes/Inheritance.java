e69d40c48be3842af7da9f3ceedcb23a7e928dc6
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-03 06:07:25 +0000

    Update Object Oriented Programming/Classes/Inheritance.java

--- Object Oriented Programming/Classes/Inheritance.java
+++ Object Oriented Programming/Classes/Inheritance.java
@@ -4,7 +4,7 @@ class Student extends Person {}
 
 class Undergraduate extends Student {}
 
-class Main {
+public class Main {
   public static void main(String[] args) {
     Student s = new Undergraduate();
     Undergraduate u = new Student(); // error
