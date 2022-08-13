a9cbee15d4d6a1b211bab5c3d7d1d46252a31f55
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-13 01:58:16 +0000

    Create  Java Stdin and Stdout I.java

---
+++ HackerRank/Prepare/Java/Introduction/Java Stdin and Stdout I.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt(); // Complete this line
+        int c = scanner.nextInt(); // Complete this line
+        scanner.close();
+
+        System.out.println(a);
+        System.out.println(b); // Complete this line
+        System.out.println(c); // Complete this line
+    }
+}
