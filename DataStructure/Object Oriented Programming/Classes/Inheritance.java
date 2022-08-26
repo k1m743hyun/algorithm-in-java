4534fa9b5e0c03692e26d49a9655adf7450d205c
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-26 18:00:34 +0900

    Update DataStructure/Object Oriented Programming/Classes/Inheritance.java

--- DataStructure/Object Oriented Programming/Classes/Inheritance.java
+++ DataStructure/Object Oriented Programming/Classes/Inheritance.java
@@ -4,9 +4,9 @@ class Student extends Person {}
 
 class Undergraduate extends Student {}
 
-public class Main {
-  public static void main(String[] args) {
-    Student s = new Undergraduate();
-    Undergraduate u = new Student(); // error
-  }
+class Main {
+    public static void main(String[] args) {
+        Student s = new Undergraduate();
+        Undergraduate u = new Student(); // error
+    }
 }
