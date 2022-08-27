3982f1b441bc2eaa65986c4aa1cf3594cf7723eb
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-27 06:57:27 +0000

    Create Java Substring Comparisons.java

---
+++ HackerRank/Prepare/Java/Strings/Java Substring Comparisons.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            String sub = s.substring(i, i + k);
+            if (i == 0) {
+                smallest = sub;
+                largest = sub;
+            } else if (sub.compareTo(smallest) < 0) {
+                smallest = sub;
+            } else if (sub.compareTo(largest) > 0) {
+                largest = sub;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
