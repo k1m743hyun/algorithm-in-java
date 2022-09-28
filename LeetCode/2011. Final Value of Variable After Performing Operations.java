b59aed3b63cf4b6b5e4ede3a9e394a46f0aae867
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-28 01:57:20 +0000

    Create LeetCode/2011. Final Value of Variable After Performing Operations.java

---
+++ LeetCode/2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String operation : operations) {
+            if (operation.charAt(0) == 'X') {
+                if (operation.charAt(1) == '+') {
+                    x += 1;
+                } else {
+                    x -= 1;
+                }
+                
+            } else {
+                if (operation.charAt(0) == '+') {
+                    x += 1;
+                } else {
+                    x -= 1;
+                }
+            }
+        }
+        return x;
+    }
+}
