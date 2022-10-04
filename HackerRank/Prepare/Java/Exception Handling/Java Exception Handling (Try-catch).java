c2b583c44c38ab9ebae3d6a590103a55d447b857
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-04 08:44:25 +0000

    Create HackerRank/Prepare/Java/Exception Handling/Java Exception Handling (Try-catch).java

---
+++ HackerRank/Prepare/Java/Exception Handling/Java Exception Handling (Try-catch).java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            scanner.close();
+            System.out.println(x / y);
+        } catch(InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch(ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
