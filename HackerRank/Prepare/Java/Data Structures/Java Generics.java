839ec382a81220eca6e7ec3ed871b18d47e45293
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-06 00:57:28 +0000

    Update HackerRank/Prepare/Java/Data Structures/Java Generics.java

--- HackerRank/Prepare/Java/Data Structures/Java Generics.java
+++ HackerRank/Prepare/Java/Data Structures/Java Generics.java
@@ -2,24 +2,34 @@ import java.io.*;
 import java.util.*;
 
 
-class Arr <T> {
-    public <T> void printArray(T[] arr) {
-        for (T a: arr) {
-            System.out.println(a);
-        }
-    }
-}
+//class Arr <T> {
+//    public <T> void printArray(T[] arr) {
+//        for (T a: arr) {
+//            System.out.println(a);
+//        }
+//    }
+//}
 
-public class Solution {
+//public class Solution {
+public class Solution <T> {
+
+    public <T> void printArray(T[] arr) {
+      for (T a: arr) {
+          System.out.println(a);
+      }
+    }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Arr arr = new Arr();
+        //Arr arr = new Arr();
+        Solution solution = new Solution<>();
         
         Integer[] a = {1, 2, 3};
-        arr.printArray(a);
+        //arr.printArray(a);
+        solution.printArray(a);
         
         String[] b = {"Hello", "World"};
-        arr.printArray(b);
+        //arr.printArray(b);
+        solution.printArray(b);
     }
 }
