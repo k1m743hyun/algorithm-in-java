91e16b01dbbccd14c3287c671fda0405de6e2e2c
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-23 21:46:32 +0900

    Create HackerRank/Prepare/Java/Advanced/Java MD5.java

---
+++ HackerRank/Prepare/Java/Advanced/Java MD5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import javax.xml.bind.DatatypeConverter;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        scanner.close();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(s.getBytes());
+            System.out.println(DatatypeConverter.printHexBinary(md.digest()).toLowerCase());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
