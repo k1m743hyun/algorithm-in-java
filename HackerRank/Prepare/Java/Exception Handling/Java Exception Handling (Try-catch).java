b874f13e84a79d8897c752dac1f5547c969fcf3c
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-31 15:38:33 +0900

    Update HackerRank/Prepare/Java/Exception Handling/Java Exception Handling (Try-catch).java

--- HackerRank/Prepare/Java/Exception Handling/Java Exception Handling (Try-catch).java
+++ HackerRank/Prepare/Java/Exception Handling/Java Exception Handling (Try-catch).java
@@ -4,12 +4,14 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         try {
             Scanner scanner = new Scanner(System.in);
             int x = scanner.nextInt();
             int y = scanner.nextInt();
             scanner.close();
+
             System.out.println(x / y);
         } catch(InputMismatchException e) {
             System.out.println(e.getClass().getName());
