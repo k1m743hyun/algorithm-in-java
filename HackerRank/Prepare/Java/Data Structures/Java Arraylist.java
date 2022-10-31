0c57663cb9e35bf72eba9bd63cd629050c299db6
Taehyun Kim <k1m743hyun@icloud.com>
2022-10-31 15:35:01 +0900

    Update HackerRank/Prepare/Java/Data Structures/Java Arraylist.java

--- HackerRank/Prepare/Java/Data Structures/Java Arraylist.java
+++ HackerRank/Prepare/Java/Data Structures/Java Arraylist.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
 
