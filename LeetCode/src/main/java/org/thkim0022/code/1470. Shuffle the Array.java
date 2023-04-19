694345a5218c827d8c6639b36872791e26685cb7
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-19 21:50:31 +0900

    Create 1470. Shuffle the Array.java

--- LeetCode/src/main/java/org/thkim0022/code/1470. Shuffle the Array.java
+++ LeetCode/src/main/java/org/thkim0022/code/1470. Shuffle the Array.java
@@ -1,27 +1,11 @@
-package org.thkim0022.code;
-
 class Solution {
+
     public int[] shuffle(int[] nums, int n) {
-        int[] x = new int[n];
-        for (int i = 0; i < n; i++) {
-            x[i] = nums[i];
+
+        int[] result = new int[n * 2];
+        for (int i = 0; i < nums.length; i++) {
+            result[((i % n) * 2) + (i / n)] = nums[i];
         }
-        
-        int[] y = new int[n];
-        for (int j = 0; j < n; j++) {
-            y[j] = nums[n+j];
-        }
-        
-        int[] result = new int[n*2];
-        for (int k = 0; k < n*2; k++) {
-            int idx = (int) k / 2;
-            if (k % 2 == 0) {
-                result[k] = x[idx];
-            } else {
-                result[k] = y[idx];
-            }
-        }
-        
         return result;
     }
 }
