71bb5dad09fca2723ee5d7e066aa2b8e74702781
Taehyun Kim <k1m743hyun@icloud.com>
2023-04-20 04:49:13 +0000

    Create 2469. Convert the Temperature.java

---
+++ LeetCode/src/main/java/org/thkim0022/code/2469. Convert the Temperature.java
@@ -0,0 +1,8 @@
+class Solution {
+
+    public double[] convertTemperature(double celsius) {
+
+        double[] result = {celsius + 273.15, celsius * 1.80 + 32.00};
+        return result;
+    }
+}
