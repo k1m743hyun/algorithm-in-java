1c6892cc0f060620f35dae739722fdac20a3b4ff
Taehyun Kim <k1m743hyun@icloud.com>
2023-05-16 17:09:39 +0900

    Update ThisIsJava/src/ch02/sec01/VariableInitializationExample.java

--- ThisIsJava/src/ch02/sec01/VariableInitializationExample.java
+++ ThisIsJava/src/ch02/sec01/VariableInitializationExample.java
@@ -4,7 +4,7 @@ public class VariableInitializationExample {
     public static void main(String[] args) {
 
         // 변수 value 선언
-        int value;
+        int value = 30;
 
         // 연산 결과를 변수 result의 초기값으로 대입
         int result = value + 10;
