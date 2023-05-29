09a7a3647236e6cbaf0b0fcad1dc321ce393c874
Taehyun Kim <k1m743hyun@icloud.com>
2023-05-29 17:21:37 +0900

    Update src/ch05/sec12/WeekExample.java

--- ThisIsJava/src/ch05/sec12/WeekExample.java
+++ ThisIsJava/src/ch05/sec12/WeekExample.java
@@ -2,6 +2,16 @@ package ch05.sec12;
 
 import java.util.Calendar;
 
+enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
 public class WeekExample {
 
     public static void main(String[] args) {
@@ -20,7 +30,7 @@ public class WeekExample {
             case 1: today = Week.SUNDAY; break;
             case 2: today = Week.MONDAY; break;
             case 3: today = Week.TUESDAY; break;
-            case 4: today = Week.WEDSNESDAY; break;
+            case 4: today = Week.WEDNESDAY; break;
             case 5: today = Week.THURSDAY; break;
             case 6: today = Week.FRIDAY; break;
             case 7: today = Week.SATURDAY; break;
