23848a1209ed6a45b21936c35a52d14cc5ea5aeb
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-02 10:06:06 +0900

    Rename the directory

--- DataStructure/src/main/java/org/k1m743hyun/Object_Oriented_Programming/Classes/Inheritance.java
+++ DataStructure/src/main/java/org/k1m743hyun/Object_Oriented_Programming/Classes/Inheritance.java
@@ -1,10 +1,12 @@
+package src.main.java.org.k1m743hyun.Object\ Oriented\ Programming.Classes;
+
 class Person {}
 
 class Student extends Person {}
 
 class Undergraduate extends Student {}
 
-class Main {
+class Inheritance {
     public static void main(String[] args) {
         Student s = new Undergraduate();
         Undergraduate u = new Student(); // error
