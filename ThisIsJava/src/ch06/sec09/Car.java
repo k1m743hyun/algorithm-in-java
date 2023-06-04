fe6e587f0f785166811df2a180b49ffab289e0c8
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-04 15:39:53 +0900

    Update ThisIsJava/src/ch06/sec09/Car.java

--- ThisIsJava/src/ch06/sec09/Car.java
+++ ThisIsJava/src/ch06/sec09/Car.java
@@ -10,4 +10,15 @@ public class Car {
     Car(String model) {
         this.model = model; // 매개변수를 필드에 대입 (this 생략 불가)
     }
+
+    // 메서드 선언
+    void setSpeed(int speed) {
+        this.speed = speed; // 매개변수를 필드에 대입 (this 생략 불가)
+    }
+
+    void run() {
+
+        this.setSpeed(100);
+        System.out.println(this.model + "가 달립니다. (시속: " + this.speed + "km/h)");
+    }
 }
