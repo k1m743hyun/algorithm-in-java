29c3ba5aad13b3b188a809ae036d0245e1937002
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-07 11:37:24 +0900

    Create ThisIsJava/src/ch06/sec13/exam02/package2/C.java

---
+++ ThisIsJava/src/ch06/sec13/exam02/package2/C.java
@@ -0,0 +1,11 @@
+package src.ch06.sec13.exam02.package2;
+
+import src.ch06.sec13.exam02.package1.*;
+
+public class C {
+
+    // 필드 선언
+    A a1 = new A(true);      // O
+    //A a2 = new A(1);         // X
+    //A a3 = new A("문자열");    // X
+}
