0e70630fe90db419ea7528856118e06f83242707
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-12 13:29:48 +0900

    Create src/ch07/sec06/package2/D.java

---
+++ ThisIsJava/src/ch07/sec06/package2/D.java
@@ -0,0 +1,27 @@
+package src.ch07.sec06.package2;
+
+import src.ch07.sec06.package1.A;
+
+public class D extends A {
+
+    // 생성자 선언
+    public D() {
+        //A() 생성자 호출
+        super();    // O
+    }
+
+    // 메서드 선언
+    public void method1() {
+        // A 필드값 변경
+        this.field = "value";   // O
+        // A 메서드 호출
+        this.method();          // O
+    }
+
+    // 메서드 선언
+    public void method2() {
+        A a = new A();      // X
+        a.field = "value";  // X
+        a.method();         // X
+    }
+}
