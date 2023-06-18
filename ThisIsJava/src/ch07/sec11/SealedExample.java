01acb2d0c994f59c6d0415a32ca387b578f9b99c
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-18 21:39:38 +0900

    Create ThisIsJava/src/ch07/sec11/SealedExample.java

---
+++ ThisIsJava/src/ch07/sec11/SealedExample.java
@@ -0,0 +1,17 @@
+package src.ch07.sec11;
+
+public class SealedExample {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        Employee employee = new Employee();
+        Manager manager = new Manager();
+        Director director = new Director();
+
+        person.work();
+        employee.work();
+        manager.work();
+        director.work();
+    }
+}
