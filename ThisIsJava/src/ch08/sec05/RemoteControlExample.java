977acc6bacd76c5e903a913916efcd0f3a9832f3
Taehyun Kim <k1m743hyun@icloud.com>
2023-06-25 20:36:35 +0900

    Create ThisIsJava/src/ch08/sec05/RemoteControlExample.java

--- ThisIsJava/src/ch08/sec05/RemoteControlExample.java
+++ ThisIsJava/src/ch08/sec05/RemoteControlExample.java
@@ -15,5 +15,16 @@ public class RemoteControlExample {
         // 디폴트 메서드 호출
         rc.setMute(true);
         rc.setMute(false);
+
+        System.out.println();
+
+        // Audio 객체를 생성하고 인터페이스 변수에 대입
+        rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+
+        // 디폴트 메서드 호출
+        rc.setMute(true);
+        rc.setMute(false);
     }
 }
