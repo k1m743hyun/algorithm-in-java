ead22f9d7aae5b0bfd6d1f858905f4d563144eab
Taehyun Kim <k1m743hyun@icloud.com>
2023-08-21 11:09:30 +0900

    Create ThisIsJava/src/ch09/sec05/exam02/AExample.java

---
+++ ThisIsJava/src/ch09/sec05/exam02/AExample.java
@@ -0,0 +1,13 @@
+package src.ch09.sec05.exam02;
+
+public class AExample {
+
+    public static void main(String[] args) {
+
+        // A 객체 생성
+        A a = new A();
+
+        // A 메서드 호출
+        a.useB();
+    }
+}
