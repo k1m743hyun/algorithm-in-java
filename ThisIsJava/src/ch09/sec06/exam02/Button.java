e91d881d74d5ca614f337d9558dd7b6f364f6502
Taehyun Kim <k1m743hyun@icloud.com>
2023-08-21 11:14:57 +0900

    Create ThisIsJava/src/ch09/sec06/exam02/Button.java

---
+++ ThisIsJava/src/ch09/sec06/exam02/Button.java
@@ -0,0 +1,20 @@
+package src.ch09.sec06.exam02;
+
+public class Button {
+
+    // 정적 멤버 인터페이스
+    public static interface ClickListener {
+
+        // 추상 메서드
+        void onClick();
+    }
+
+    // 필드
+    private ClickListener clickListener;
+
+    // 메서드
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+}
+
