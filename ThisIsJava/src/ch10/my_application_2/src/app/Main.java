1c54a1c509ca5aa383ed8554e182aa437a761f72
Taehyun Kim <k1m743hyun@icloud.com>
2023-08-22 09:51:56 +0900

    Update ThisIsJava/src/ch10/my_application_2

--- ThisIsJava/src/ch10/my_application_2/src/app/Main.java
+++ ThisIsJava/src/ch10/my_application_2/src/app/Main.java
@@ -1,7 +1,7 @@
 package app;
 
 import pack1.A;
-import pack2.B;
+//import pack2.B;
 import pack3.C;
 
 public class Main {
@@ -13,11 +13,14 @@ public class Main {
         a.method();
 
         // my_module_a 패키지에 포함된 B 클래스 이용
-        B b = new B();
-        b.method();
+        //B b = new B();
+        //b.method();
 
         // my_module_b 패키지에 포함된 C 클래스 이용
         C c = new C();
         c.method();
+
+        C result = a.getC();
+        result.method();
     }
 }
--- ThisIsJava/src/ch10/my_application_2/src/module-info.java
+++ ThisIsJava/src/ch10/my_application_2/src/module-info.java
@@ -1,4 +1,4 @@
 module my_application_2 {
     requires my_module_a;
-    requires my_module_b;
+    //requires my_module_b;
 }
