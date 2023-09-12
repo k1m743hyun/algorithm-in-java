bcdc97773fb42b156602f2076612e7a2d119424f
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-12 10:09:32 +0900

    Update ThisIsJava/src/ch12/sec03/exam05/Member.java

--- ThisIsJava/src/ch12/sec03/exam05/Member.java
+++ ThisIsJava/src/ch12/sec03/exam05/Member.java
@@ -1,7 +1,11 @@
 package src.ch12.sec03.exam05;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Member {
     private String id;
