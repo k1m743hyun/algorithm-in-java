ce7aa4f8bc2959890ff0f7f495b7d3e761f6471a
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-14 11:15:45 +0900

    Update ThisIsJava/src/ch12/sec09/SimpleDateFormatExample.java

--- ThisIsJava/src/ch12/sec09/SimpleDateFormatExample.java
+++ ThisIsJava/src/ch12/sec09/SimpleDateFormatExample.java
@@ -9,7 +9,7 @@ public class SimpleDateFormatExample {
 
         Date now = new Date();
 
-       String[] patternList = {"yyyy-MM-dd", "yyyy년 MM월 dd일", "yyyy.MM.dd a HH:mm:ss", "오늘은 E요일", "올해의 D번째 날", "이달의 d번째 날"};
+        String[] patternList = {"yyyy-MM-dd", "yyyy년 MM월 dd일", "yyyy.MM.dd a HH:mm:ss", "오늘은 E요일", "올해의 D번째 날", "이달의 d번째 날"};
         SimpleDateFormat sdf;
         for (String pattern : patternList) {
             sdf = new SimpleDateFormat(pattern);
