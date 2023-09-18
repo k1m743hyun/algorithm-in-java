538e932b6a2175ed4f7770eef9fc447cc1b2b211
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-18 18:32:12 +0900

    Create ThisIsJava/src/ch14/sec07/exam02/PrintThread.java

---
+++ ThisIsJava/src/ch14/sec07/exam02/PrintThread.java
@@ -0,0 +1,18 @@
+package src.ch14.sec07.exam02;
+
+public class PrintThread extends Thread {
+
+    public void run() {
+
+        try {
+            while (true) {
+                System.out.println();
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("리소스 정리");
+        System.out.println("실행 종료");
+    }
+}
