e39b36feef18afe50e806c8a09103f04ce3a59f8
Taehyun Kim <k1m743hyun@icloud.com>
2023-09-25 16:44:17 +0900

    Create ThisIsJava/src/ch17/sec07/exam01/Student.java

---
+++ ThisIsJava/src/ch17/sec07/exam01/Student.java
@@ -0,0 +1,25 @@
+package src.ch17.sec07.exam01;
+
+public class Student implements Comparable<Student> {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(score, o.score);
+    }
+}
