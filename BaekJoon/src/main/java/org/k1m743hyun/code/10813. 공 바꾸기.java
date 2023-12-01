9a1fe9a2b3c1c4b1c5af8f70726e61f4a8af6336
Taehyun Kim <k1m743hyun@icloud.com>
2023-12-01 21:34:54 +0900

    Create 10813. 공 바꾸기.java

---
+++ BaekJoon/src/main/java/org/k1m743hyun/code/10813. 공 바꾸기.java
@@ -0,0 +1,35 @@
+package src.main.java.org.k1m743hyun.code;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // input
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        // logic
+        int[] answer = new int[n];
+        for (int x = 0; x < n; x++) {
+            answer[x] = x + 1;
+        }
+
+        for (int y = 0; y < m; y++) {
+            int i = scanner.nextInt();
+            int j = scanner.nextInt();
+
+            int temp = answer[i - 1];
+            answer[i - 1] = answer[j - 1];
+            answer[j - 1] = temp;
+        }
+
+        // output
+        for (int z : answer) {
+            System.out.println(z);
+        }
+    }
+}
